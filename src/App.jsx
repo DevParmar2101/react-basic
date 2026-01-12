e4aa0fa09b80f4d00588defab687def963b3e7a5
Dev Parmar <devp2101@gmail.com>
2026-01-12 17:05:05 +0530

    - Learn use-effect in react

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import UseState from "./pages/UseState";
 import Props from "./pages/Props.jsx";
 import List from "./pages/ListRender.jsx";
 import Mini from "./pages/MiniTask.jsx";
+import UseEffect from "./pages/UseEffect.jsx";
+
 import './App.css'
 
 function App() {
     return (
         <BrowserRouter>
             <nav style={{ display: "flex", "gap": "10px" }}>
-                <Link to="/">Home</Link>
+                <Link to="/">Home</Link> |{" "}
                 <Link to="/state">UseState</Link>
                 <Link to="/props">Props</Link>
                 <Link to="/list">List</Link>
                 <Link to="/task">Mini Task</Link>
+                <Link to="/use-effect">UseEffect</Link>
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
@@ -23,6 +26,7 @@ function App() {
                 <Route path="/props" element={<Props />} />
                 <Route path="/list" element={<List />} />
                 <Route path="/task" element={<Mini />} />
+                <Route path="/use-effect" element={<UseEffect />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/pages/UseEffect.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+function UseEffect() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    ///Runs once when component loads
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((data) => {
+                setUsers(data);
+                setLoading(false);
+            })
+    }, []); //empty array = run only once
+
+    return (
+        <div>
+            <h1>useEffect Example</h1>
+
+            { loading && <p>Loading...</p> }
+
+            {! loading && (
+                <ul>
+                    {users.map((user) => (
+                        <li key={user.id}>
+                            {user.name} - {user.email}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
+
+export default UseEffect;
