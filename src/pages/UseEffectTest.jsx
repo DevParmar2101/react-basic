03df23d62f89208d1eaadcc10fe01d2b430c37b1
Dev Parmar <devp2101@gmail.com>
2026-01-13 12:54:05 +0530

    - Reload button added

--- src/pages/UseEffectTest.jsx
+++ src/pages/UseEffectTest.jsx
@@ -1,29 +1,39 @@
 import {useEffect, useState} from "react";
 
 function UseEffectTest() {
+
     const [user, setUser] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [reload, setReload] = useState(0);
 
     useEffect(() => {
+        setLoading(true);
+
         fetch("https://jsonplaceholder.typicode.com/posts")
             .then((res) => res.json())
             .then((data) => {
-            setUser(data);
-            setLoading(false);
-        })
-    }, []);
+                setUser(data);
+                setLoading(false);
+            })
+    }, [reload]);
 
+    function reloadPost() {
+        setReload((prev) => prev + 1); // 👈 always changes
+    }
     return (
         <div>
             <h2> useEffect Test</h2>
             { loading &&  <p>Loading...</p>}
 
             {! loading && (
-                <ol>
-                    {user.map((item) => (
-                        <li key={item.id}>{item.title}</li>
-                    ))}
-                </ol>
+                <div>
+                    <button onClick={reloadPost}>Reload Post</button>
+                    <ol>
+                        {user.map((item) => (
+                            <li key={item.id}>{item.title}</li>
+                        ))}
+                    </ol>
+                </div>
             )}
         </div>
     );
